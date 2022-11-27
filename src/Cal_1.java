31e05eb14e32c89d586c95e5a118f19b40a03fe3
JUn <JUn@DESKTOP-PPJ58M7>
2022-11-27 13:33:54 +0900

    initial commit

---
+++ src/Cal_1.java
@@ -0,0 +1,9 @@
+
+public class Cal_1 {
+	public static void main(String[] args) {
+		System.out.println("µ¡¼À : " + (3 + 4));
+		System.out.println("»¬¼À : " + (7 - 4));
+		System.out.println("°ö¼À : " + (3 * 4));
+		System.out.println("³ª´°¼À : " + (4 / 2));
+	}
+}
